8c571a40bd7462ae2db6a12499c41923c791591c
Benjamin Powell <benjaminpowell24@gmail.com>
2025-01-30 11:52:05 +0000

    fix: display correct port application is running on

--- backend-2/index.js
+++ backend-2/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
-app.use('/v2', appRoutes)
+app.use('/v1', appRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
